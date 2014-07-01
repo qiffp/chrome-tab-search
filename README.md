8dea33b16c76831dec11b6cee3e06355af44e444
Sam Dye <sam.re.dye@gmail.com>
2014-07-01 14:35:49 -0400

    Add tab favicon to each cell

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+## WIP
+
 chrome-tab-search
 =================
 
--- popup.js
+++ popup.js
@@ -1,16 +1,27 @@
-function makeTabListWithArray(tabs, activeTabIndex) {
+function makeTabListWithArray(tabs, activeTabId) {
   var list = document.createElement('ul');
   list.id = 'results_list';
 
   for (var i = 0; i < tabs.length; i++) {
     var item = document.createElement('li');
     item.className = 'result';
-    if (i === activeTabIndex) {
+
+    if (tabs[i].favIconUrl) {
+      var favicon = document.createElement('img');
+      favicon.setAttribute('src', tabs[i].favIconUrl);
+      item.appendChild(favicon);
+    } else {
+      var padding = document.createElement('div');
+      padding.setAttribute('class', 'empty-favicon-padding');
+      item.appendChild(padding);
+    }
+
+    if (tabs[i].id === activeTabId) {
       var boldElement = document.createElement('b');
-      boldElement.appendChild(document.createTextNode(tabs[i]));
+      boldElement.appendChild(document.createTextNode(tabs[i].title));
       item.appendChild(boldElement);
     } else {
-      item.appendChild(document.createTextNode(tabs[i]));
+      item.appendChild(document.createTextNode(tabs[i].title));
     }
 
     list.appendChild(item);
@@ -30,20 +41,11 @@ document.addEventListener('DOMContentLoaded', function() {
       return previous.concat(current.tabs);
     }, []);
 
-    var titles = [];
     var activeTabId;
     var activeTabIndex;
     chrome.tabs.query({active: true, currentWindow: true}, function(activeTabs) {
       activeTabId = activeTabs[0].id;
-
-      for (var i = 0; i < tabs.length; i++) {
-        titles[i] = tabs[i].title;
-        if (tabs[i].id === activeTabId) {
-          activeTabIndex = i;
-        }
-      }
-
-      resultsSection.appendChild(makeTabListWithArray(titles, activeTabIndex));
+      resultsSection.appendChild(makeTabListWithArray(tabs, activeTabId));
       resultsList = resultsSection.lastChild;
     });
   });
